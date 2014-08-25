c9c92ab801f8827b90da96263aab62662f0764e1
athuls89 <athuls89@gmail.com>
2014-08-25 01:08:24 -0400

    Changing reference to Vincenty

--- senstore/contextInterpretation/field_inspector_trail/ApproxApproachAcrossPlanet/AnalyzeLatLongVariation.pl
+++ senstore/contextInterpretation/field_inspector_trail/ApproxApproachAcrossPlanet/AnalyzeLatLongVariation.pl
@@ -1,22 +1,24 @@
 #!/usr/bin/perl
+use warnings;
 
-my $windowSize = 5;
+my $windowSize = 10;
 my @places = ("Athens", "Beijing","Cairo","Frankfurt","HongKong","Lisbon","London","Madrid","Melbourne","Moscow","Mumbai","NewDelhi","Paris","Rio","Rome","Shanghai","Tehran","Tokyo","Vienna","Zurich");
-open FO, ">LatLongVariation";
+open FO, ">"."VincentyDistances/LatLongVariation.csv";
 foreach(@places)
 {
 my $averageWindowError = 0;
 my $count = 0;	
 my $place = $_;
 open FI, "<$place"."Calibration";
-open FIOutput, "<$place"."FinalError.csv";
+open FIOutput, "<"."VincentyDistances/$place"."FinalError.csv";
 while(<FI>)
 {
 	my @splitLine = split(",", $_);
-	if($splitLine[0] eq "Latitude")
+	if($splitLine[0] eq "Origin Latitude")
 	{
 		my $latLong = <FI>;
 		chomp $latLong;
+		chop $latLong;
 		print FO $latLong.",";
 		last;
 	}
@@ -32,7 +34,7 @@ while(<FIOutput>)
 	if($count == $windowSize)
 	{
 		$averageWindowError = $averageWindowError / $windowSize;
-		if($averageWindowError > 0.1)
+		if($averageWindowError > 0.00621371)
 		{
 			print FO $splitLine[0]."\n";
 			last;
