913ea66120d5dac391000895fd21a591c10d3f08
athuls89 <athuls89@gmail.com>
2014-09-30 05:35:03 -0400

    Updating with HongKong for perf analysis

--- senstore/contextInterpretation/codes_MCMC/SamplePositions/src/vincenti/Vincenti.java
+++ senstore/contextInterpretation/codes_MCMC/SamplePositions/src/vincenti/Vincenti.java
@@ -53,9 +53,9 @@ public class Vincenti
 		bridge_origin=new double[3];
 		
 		// Add latitude/longitude coordinates with sign, beginning with longitude
-		bridge_origin[0]=31.23571160000006; //compute the bridge origin in GPS
-		bridge_origin[1]=30.0444196;
-		bridge_origin[2]=26.7;
+		bridge_origin[0]=114.1094970000; //compute the bridge origin in GPS
+		bridge_origin[1]=22.396428;
+		bridge_origin[2]=321.1;
 		BFR_roll=0;
 		BFR_pitch=0;
 		BFR_yaw=-61.237;
@@ -278,7 +278,7 @@ public class Vincenti
 		double linearDistance = temp1.computeIO_BFR();
 		temp1.computeIFRorient_BFR();
 		*/
-		String city = "Cairo";
+		String city = "HongKong";
 			
 		BufferedReader in = new BufferedReader(new FileReader("/mnt/sdb/old/opt/umich-panther/senstore/contextInterpretation/field_inspector_trail/ApproxApproachAcrossPlanet/" + city + "LocLinearInput"));
 		BufferedWriter out = new BufferedWriter(new FileWriter("/mnt/sdb/old/opt/umich-panther/senstore/contextInterpretation/field_inspector_trail/ApproxApproachAcrossPlanet/VincentyDistances/output_"+city+"VincentyDistancesTest"));
